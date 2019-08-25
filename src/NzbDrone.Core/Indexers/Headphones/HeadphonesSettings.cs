69f9673392924b59f95d4af269a78a6d742e33f6
Taloth Saldono <Taloth@users.noreply.github.com>
2019-08-25 22:00:41 +0100

    Updated FluentValidation to 8.4.0

--- src/NzbDrone.Core/Indexers/Headphones/HeadphonesSettings.cs
+++ src/NzbDrone.Core/Indexers/Headphones/HeadphonesSettings.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using FluentValidation;
-using FluentValidation.Results;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Annotations;
 using NzbDrone.Core.Validation;
@@ -11,14 +10,12 @@ namespace NzbDrone.Core.Indexers.Headphones
     {
         public HeadphonesSettingsValidator()
         {
-            Custom(newznab =>
+            RuleFor(c => c).Custom((c, context) =>
             {
-                if (newznab.Categories.Empty())
+                if (c.Categories.Empty())
                 {
-                    return new ValidationFailure("", "'Categories' must be provided");
+                    context.AddFailure("'Categories' must be provided");
                 }
-
-                return null;
             });
 
             RuleFor(c => c.Username).NotEmpty();
